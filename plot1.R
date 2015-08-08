26ed5d5c598472a12b0879f637731ef591a0f0fb
kaka <kakahwei87@hotmail.com>
2015-08-09 01:56:42 +0800

    Third version

--- plot1.R
+++ plot1.R
@@ -1,15 +1,15 @@
 ##Download Dataset: Electric power consumption [20Mb] , put it in particular folder.
-##Modify the working directory to the folder with dataset  
+##Modify the working directory to the folder with dataset "Electric power consumption"
 setwd("D:/#Coursera Material/R_workingDir/M4/courseProjectOne")
 
 library(sqldf) ## load library
 
 powerFile <- file("./household_power_consumption.txt")
 attr(powerFile, "file.format") <- list(sep = ";", header = TRUE) 
-powerList2 <- sqldf("select * from powerFile where Date = '1/2/2007' or Date = '3/2/2007' ") 
+powerList <- sqldf("select * from powerFile where Date = '1/2/2007' or Date = '2/2/2007' ") 
 
 png(filename = "plot1.png", width=480, heigh =480, units="px" , bg="white")
-powerHist <- hist(powerList2$Global_active_power,
+hist(powerList$Global_active_power,
      xlab= "Global Active Power(kilowatts)", 
      main="Global Active Power",  
      col="Red",
