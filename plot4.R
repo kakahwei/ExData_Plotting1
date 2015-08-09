c289687dda49c0bf682725fa6c6a6fa3f740c171
kaka <kakahwei87@hotmail.com>
2015-08-09 12:43:56 +0800

    Second version

--- plot4.R
+++ plot4.R
@@ -48,7 +48,7 @@ lines( powerList$Sub_metering_3,
        lwd=1)
 
 nameList <- names(powerList)
-legend(x="topright", legend=nameList[7:9],col=c(1:3),lty = 1)
+legend(x="topright", legend=nameList[7:9],col=c("black", "red","blue"),lty = 1)
 axis(side = 1, at = c(0, 1500,2500), labels = c("Thu","Fri","Sat"))
 
 ## Plot number 4
